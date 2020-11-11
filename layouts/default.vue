75158d9b5886431df0b9d5e296971c6400a74e27
majordutch <354294+majordutch@users.noreply.github.com>
2020-11-11 10:56:00 -0800

    Change main column size back to previous

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section class="main-content columns">
-      <div class="column is-10 is-offset-1">
+      <div class="column is-8 is-offset-2">
         <nuxt />
       </div>
     </section>
