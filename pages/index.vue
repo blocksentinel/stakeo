ac34575f7b6301507b7458fdf9a362cf3c972176
majordutch <354294+majordutch@users.noreply.github.com>
2020-11-17 21:08:17 -0800

    Remove notice

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,5 @@
 <template>
   <section class="section">
-    <b-message
-      aria-close-label="Close message"
-      title="Notice"
-      type="is-warning"
-    >
-      This tool is in beta, it may break unexpectedly and/or report incorrect
-      numbers at times. No warranty, not financial advice.
-    </b-message>
     <div class="columns">
       <div class="column is-half-desktop">
         <Calculator />
