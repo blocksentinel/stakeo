8a583c18956b2ed682664337123c952b0ba13066
majordutch <354294+majordutch@users.noreply.github.com>
2021-03-01 00:00:20 -0800

    Show 3 decimals

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -311,15 +311,15 @@ export default {
     formatVolume(val) {
       if (val > 999999999999) {
         return this.$t('calculator.volumeCompactTrillion', {
-          value: (val / 1000000000000).toFixed(1),
+          value: (val / 1000000000000).toFixed(3),
         })
       } else if (val > 999999999) {
         return this.$t('calculator.volumeCompactBillion', {
-          value: (val / 1000000000).toFixed(1),
+          value: (val / 1000000000).toFixed(3),
         })
       } else {
         return this.$t('calculator.volumeCompactMillion', {
-          value: (val / 1000000).toFixed(1),
+          value: (val / 1000000).toFixed(3),
         })
       }
     },
